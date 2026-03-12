d7a944c91c6cd821474eacc8ad0ea3623cb4618e
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2026-03-12 16:05:03 +0200

    remove unneccessary explicit imports in dunder init

--- src/whatdeps/__init__.py
+++ src/whatdeps/__init__.py
@@ -1,6 +1,1 @@
-__version__ = "0.1.0"
-
-from .inspector import PackageInspector
-from .models import Origin, PackageInfo
-
-__all__ = ["Origin", "PackageInfo", "PackageInspector"]
+__version__ = "0.1.1"
